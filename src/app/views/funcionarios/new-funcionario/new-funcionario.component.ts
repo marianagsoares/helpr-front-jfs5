38ecc498cd75fce359a7c09e58ca24594786a3be
marianagsoares <marianasoares.ti@gmail.com>
2022-12-09 12:54:28 -0300

    criação de pipe para foto do componente usuario

---
+++ src/app/views/funcionarios/new-funcionario/new-funcionario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-funcionario',
+  templateUrl: './new-funcionario.component.html',
+  styleUrls: ['./new-funcionario.component.css']
+})
+export class NewFuncionarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
